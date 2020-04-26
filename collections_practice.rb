84c5086fe489d0ec9d71001e702dbd49de147990
Cailie Skelton <cskelton707@gmail.com>
2020-04-26 16:58:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,3 +31,8 @@ end
 sort_array_char_count(["blake","ashley","scott"])
 
 
+def sort_array_swap_elements(array)
+  array[1], array[2] = array[2], array [1]
+  array
+end
+sort_array_swap_elements(["Metropolis","Amadeus","Wayne's World"]) 
